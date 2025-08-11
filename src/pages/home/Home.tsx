29242da79d5e4cb6cca972c816993f5de8a385ac
clemersoncaetano <clemersoncaetano346@gmail.com>
2025-08-11 19:44:36 -0300

    home sendo feita

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useNavigate } from "react-router-dom"
+import style from "./style.module.css"
+
+export const home=()=> {
+  const navigate = useNavigate()
+
+
+
+
+  return<>
+<main>
+
+ return <>
+
+ 
+            <main>
+
+                <div className={style.div}>
+                <div className={style.cupcakeLeft}>🧁</div>
+      
+      <div className={style.titleContainer}>
+        <h1 className={style.title}>Movies</h1>
+        <p className={style.subtitle}>🔴 Filmes 🔴</p>
+      </div>
+      
+     
+
+
+
+                </div>
+                  
+      
+      
+            </main>
+        </>
+
+
+</main>
+
+
+
+
+  </>
+}
