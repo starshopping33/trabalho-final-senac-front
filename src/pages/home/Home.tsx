de17de34e81c5ef3c575340e89c752f4b5b02e0b
starshpping333 <starshopping3333@gmail.com>
2025-08-29 20:11:36 -0300

    Casa Kauan

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import style from "./style.module.css"
 import { apiController } from "../../controller/api.controller"
 import { Poster } from "../../components/poster/Poster"
+import { useNavigate } from "react-router-dom"
 
 interface Filme {
   backdrop_path: string,
@@ -9,40 +10,64 @@ interface Filme {
   title:string
 }
 
+
 export const Home=()=> {
   const [filmes,setFilmes]= useState([] as Filme[])
+  const [page,setPage] = useState(1)
   const getFilmes=async() => {
-    const res = await apiController.getFilmes()
+    const res = await apiController.getFilmes(String(page))
     console.log(res,"res")
     setFilmes(res.results)
   }
+
+  const navigate = useNavigate();
   
 
   useEffect(()=>{
     getFilmes()
   },[])
-
-
+  useEffect(()=>{
+    getFilmes()
+  },[page])
+const goTo=(filme:Filme)=>{
+  console.log(filme,"filme?")
+  //navigate(`/Informacoes`)
+  //document.documentElement.style.setProperty('--bg-color', '#ff5733')
+}
  return <>
 <section>
   <div className={style.div}>
-<h1 className={style.title}>Movies</h1>
-<p className={style.subtitle}>🔴 Filmes 🔴</p>
+
+<p className={style.subtitle}> Filmes</p>
 <div className={style.lenght} >
 
 <ul>
 
   {filmes.map((filme)=>{
-    return <li>
-      <p className={style.title}>{filme.title}</p>
-        <img src={'https://image.tmdb.org/t/p/w500/'+filme.poster_path} alt="" />
-      
-      <Poster/>
+    return <li className={style.li}>
+
+        
+        <div className={style.card}> 
+          <img  className={style.img}  src={'https://image.tmdb.org/t/p/w500/'+filme.poster_path} alt=""
+           onClick={()=>goTo(filme)} />
+          <div className={style.info}>
+            <p className={style.title}>{filme.title}</p>
+            <Poster/>
+
+          </div>
+           
+      </div>
     </li>
   })}
+  
 </ul>
 </div>
-<p className={style.subtitle}>🔴 login 🔴</p>
+<div  className={style.buttonprev}>
+<button className={style.buttonprev} onClick={()=>setPage(page -1)}>Voltar</button>
+<button className={style.buttonprev} onClick={()=>setPage(page +1)}>Proximas</button>
+
+</div>
+
 
 </div>
 
